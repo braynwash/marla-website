f534c98e261c0a669c8c19c687bf8c037ccdaf08
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-02-20 14:25:04 -0500

    Nav
    Got basic navigation working and base about page.

---
+++ src/lib/+nav.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "./nav.css"
+</script>
+
+<nav class="mainNav">
+    <ul>
+      <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+    </ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { children } = $props();
+	import Nav from '$lib/+nav.svelte';
+	import "../global.css"
+
+</script>
+
+<Nav />
+
+{@render children()}
