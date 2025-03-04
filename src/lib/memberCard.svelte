700caabe13808f6558927868f364123ff1395f44
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-03-03 21:30:53 -0500

    About and Member card
    I made member card component and did a lot of the about page so far.

---
+++ src/lib/memberCard.svelte
@@ -0,0 +1,16 @@
+<script>
+	import './memberCard.css';
+</script>
+
+<div class="member">
+	<div class="img"></div>
+	<div class="memberInfo">
+		<h3>Name</h3>
+		<h4>Designation</h4>
+		<p>"Quote"</p>
+		<p>
+			The lab, in the Information Communication Technology Complex The lab, in the Information
+			Communication Technology Complex The lab, in the Information Communication Technology Complex.
+		</p>
+	</div>
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,59 @@
 <script>
-    import "./about.css";
+	import './about.css';
+	import MemberCard from '$lib/memberCard.svelte';
 </script>
 
 <main class="about">
-    <h1>About</h1>
+	<img
+		src="/about-hero.png"
+		alt="Image of Haven holding ribbons in Marla with Powers by his side."
+	/>
+	<div class="ab">
+		<h1>About Us</h1>
+		<p>
+			The lab, in the Information Communication Technology Complex Building at IU Indianapolis, has
+			Nintendo consoles ranging from the original Nintendo Entertainment System to the Wii U. Luddy
+			students can play classics such as Pac-Man and Space Invaders on an Atari 2600, which was
+			released in 1977. Classic consoles are also on display, such as the Commodore VIC-20 from 1980
+			or Radio Shack’s TRS-80, a desktop microcomputer also launched in 1977.The lab, in the
+			Information Communication Technology Complex Building at IU Indianapolis, has Nintendo
+			consoles ranging from the original Nintendo Entertainment System to the Wii U.
+		</p>
+		<p>
+			Luddy students can play classics such as Pac-Man and Space Invaders on an Atari 2600, which
+			was released in 1977. Classic consoles are also on display, such as the Commodore VIC-20 from
+			1980 or Radio Shack’s TRS-80, a desktop microcomputer also launched in 1977.The lab, in the
+			Information Communication Technology Complex Building at IU Indianapolis, has Nintendo
+			consoles ranging from the original Nintendo Entertainment System to the Wii U. Luddy students
+			can play classics such as Pac-Man and Space Invaders on an Atari 2600, which was released in
+			1977. Classic consoles are also on display, such as the Commodore VIC-20 from 1980 or Radio
+			Shack’s TRS-80, a desktop microcomputer also launched in 1977.
+		</p>
+	</div>
+
+	<div class="about-pics">
+		<div class="about-pic">
+			<img src="/about-pic1.png" alt="Picture of Nintendo Super" />
+			<p>The lab, in the Information Communication Technology Complex</p>
+		</div>
+		<div class="about-pic">
+			<img src="/about-pic2.png" alt="Picture of Nintendo Super" />
+			<p>The lab, in the Information Communication Technology Complex</p>
+		</div>
+		<div class="about-pic">
+			<img src="/about-pic3.png" alt="Picture of Nintendo Super" />
+			<p>The lab, in the Information Communication Technology Complex</p>
+		</div>
+	</div>
+	<div class="ab">
+		<h1>Meet Our Members</h1>
+		<div class="members">
+			<MemberCard />
+			<MemberCard />
+		</div>
+		<div class="members">
+			<MemberCard />
+			<MemberCard />
+		</div>
+	</div>
 </main>
