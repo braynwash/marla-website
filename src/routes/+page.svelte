f3dab7f53cb763ccb77e80348b6c00785cbd0c24
jeremy !! <f.tzgeraldz@gmail.com>
2025-03-05 17:32:03 -0500

    Started home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,28 @@
 </script>
 
 <main class="home">
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <div class="hero">
+        <div class="heroText">
+            <img alt="ROM Logo" src="/rom-logo.png">
+            <h1>PRESERVE. STUDY. PLAY.</h1>
+            <p>where people can do really scholarly study on game design to be able to look every game with this genre, mechanic, etcetera, and actually be able to play them, study them, or write papers on them.</p>
+            <a href="/collections">view archives</a>
+        </div>
+        <img id="controller" alt="3D render of a Game Controller" src="/controller.png">
+        <img id="banner" alt="border" src="/marla-banner.png" >
+    </div>
+
+<div class="homeContent">
+    <div class="homeAbout">
+        <div class="homeAbtImg">
+            <img alt="Friends" src="/about-pic3.png">
+        </div>
+        <div class="homeAbtText">
+             <h1 style="font-size: 18px; font-weight: normal;">The lab, in the Information Communication Technology Complex Building at IU Indianapolis, has Nintendo consoles ranging from the original Nintendo Entertainment System to the Wii U. Luddy students can play classics such as Pac-Man and Space Invaders on an Atari 2600, which was released in 1977. Classic consoles are also on display, such as the Commodore VIC-20 from 1980 or Radio Shack’s TRS-80, a desktop microcomputer also launched in 1977.</h1>
+            <a href="/about">About Us</a>
+        </div>
+    </div>
+</div>
+
 </main>
 
