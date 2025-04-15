6359aa2f84cbd41ba5d5d071b1eb84b002796358
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-04-14 22:43:00 -0400

    More details  for each page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,13 @@
 <script>
+	import './blogSlug.css';
 	let { data } = $props();
 </script>
 
-<h1>{data.post.title}</h1>
-<div>{@html data.post.content}</div>
+<main class="blogSlug">
+	<h1>{data.post.title}</h1>
+	<h2>{data.post.description}</h2>
+	<p>{data.post.date}</p>
+	{#if data.post.image}
+		<img src={data.post.image} alt={data.post.title} />
+	{/if}
+</main>
