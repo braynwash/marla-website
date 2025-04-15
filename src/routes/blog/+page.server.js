378301fce6eda11f48dd223be169d0ad5f66d004
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-04-14 22:10:17 -0400

    Hotfix
    Fixed bug where only titles and slugs were loading for blogposts.

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -4,7 +4,10 @@ export function load() {
 	return {
 		summaries: posts.map((post) => ({
 			slug: post.slug,
-			title: post.title
+			title: post.title,
+			image: post.image,
+			date: post.date,
+			description: post.description
 		}))
 	};
 }
