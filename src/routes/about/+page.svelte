2d7853b3267046e4359cc3924e4f029212be8431
jeremy !! <f.tzgeraldz@gmail.com>
2025-04-24 14:24:35 -0400

    Added variables to "MemberCard" component, added basic member content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -50,11 +50,12 @@
 	<div class="ab">
 		<h1>Meet Our Members</h1>
 		<div class="members">
-			<MemberCard />
-			<MemberCard />
-
-			<MemberCard />
-			<MemberCard />
+			<MemberCard name="Mathew Powers" />
+			<MemberCard image="/member-haven.jpg" name="Haven Hamelin"/>
+			<MemberCard image="/member-sam.png" name="Sam Barks" role="Council Member, Student" quote="Game Preservation to me is something we should all work towards." about="I am a full time student at IU Indy working on the Council for the Marla Museum. I love collecting anything video games and showing people how important it is to do so!"/>
+			<MemberCard image="member-gary.png" name="Gary Maxiner III"/>
+			<MemberCard image="member-jason.jpg" name="Jason Peercy"/>
+			<MemberCard image="member-zeb.jpg" name="Zebulun Wood" />
 		</div>
 	</div>
 </main>
