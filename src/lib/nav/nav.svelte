242aea7c5852bcc176462fdaee6d793613363bb0
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-05-07 13:58:00 -0400

    Mob-nav fixed again and adjustment to blog page

--- src/lib/nav/nav.svelte
+++ src/lib/nav/nav.svelte
@@ -25,13 +25,13 @@
 	<!-- The class below uses list elements around the anchor tags that serve as a "wrapper" for styling. If you wish to add a new page to the nav bar, add a new <li> element with an <a> tag inside. The href should lead to the PAGES FILE NAME with a backslash in front. -->
 	<div class="nav-links" class:open={isOpen}>
 		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/collections">Collections</a></li>
-			<li><a href="/blog">Blog</a></li>
-			<li><a href="/press">Press Releases</a></li>
+			<li><a href="/" on:click={(isOpen = !isOpen)}>Home</a></li>
+			<li><a href="/about" on:click={(isOpen = !isOpen)}>About</a></li>
+			<li><a href="/collections" on:click={(isOpen = !isOpen)}>Collections</a></li>
+			<li><a href="/blog" on:click={(isOpen = !isOpen)}>Blog</a></li>
+			<li><a href="/press" on:click={(isOpen = !isOpen)}>Press Releases</a></li>
 			<!--The contact a tag has extra styling and requires a class be on it for specificty.-->
-			<li class="contact"><a href="/contact">Contact</a></li>
+			<li class="contact"><a href="/contact" on:click={(isOpen = !isOpen)}>Contact</a></li>
 		</ul>
 	</div>
 </nav>
