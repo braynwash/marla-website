0712ac79bc96025ed01075fed7e7a59c2375bf1d
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-05-07 13:05:38 -0400

    Mobile Nav

--- src/lib/nav/nav.svelte
+++ src/lib/nav/nav.svelte
@@ -1,10 +1,9 @@
 <script>
 	import './nav.css';
+	let isOpen = false;
 </script>
 
 <nav class="mainNav">
-
-	<!-- The class below contains the MARLA logo from March 2025. -->
 	<div class="iu-logo">
 		<img
 			src="/iu-marla-logo.png"
@@ -12,28 +11,27 @@
 		/>
 	</div>
 
+	<button
+		class="hamburger"
+		aria-label="Toggle menu"
+		aria-expanded={isOpen}
+		on:click={() => (isOpen = !isOpen)}
+	>
+		<div class="bar"></div>
+		<div class="bar"></div>
+		<div class="bar"></div>
+	</button>
+
 	<!-- The class below uses list elements around the anchor tags that serve as a "wrapper" for styling. If you wish to add a new page to the nav bar, add a new <li> element with an <a> tag inside. The href should lead to the PAGES FILE NAME with a backslash in front. -->
-	<div class="links">
+	<div class="nav-links" class:open={isOpen}>
 		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="/about">About</a>
-			</li>
-			<li>
-				<a href="/collections">Collections</a>
-			</li>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/press">Press Releases</a>
-			</li>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/collections">Collections</a></li>
+			<li><a href="/blog">Blog</a></li>
+			<li><a href="/press">Press Releases</a></li>
 			<!--The contact a tag has extra styling and requires a class be on it for specificty.-->
-			<li class="contact"> 
-				<a href="/contact">Contact</a>
-			</li>
+			<li class="contact"><a href="/contact">Contact</a></li>
 		</ul>
 	</div>
 </nav>
